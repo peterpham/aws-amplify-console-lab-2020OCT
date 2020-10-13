bcbd5703e29cda8c15401632d755f6ebdfd480bd
Peter Pham <vdung.peter@gmail.com>
2020-10-13 15:03:02 +1100

    Update App.js
    update on develop branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           phampp
+           phampp - this is my develop branch
           </p>
           <a
             className="App-link"
